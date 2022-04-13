e46e7b821d1d1420f4d09508af8330288fd30b35
Dave Lucia <davelucianyc@gmail.com>
2022-04-13 08:33:32 -0400

    Start thinking about APIs

--- lib/marx.ex
+++ lib/marx.ex
@@ -1,18 +1,26 @@
 defmodule Marx do
   @moduledoc """
-  Documentation for `Marx`.
+  A Markdown compiler for the people.
+
+  Provides tools for translating [Gruber Markdown](https://daringfireball.net/projects/markdown/)
+  into various intermediate forms
+
+  * `Marx.to_ast/2` - Converts `Markdown (text)` -> `Marx.AST`
+  * `Marx.AST.to_html_ast/2` - Converts `Mark.AST` -> `Marx.HTML.AST`
+  * `Marx.HTML.to_html/2` - Converts `Marx.HTML.AST` -> `HTML (text)`
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Marx.hello()
-      :world
 
   """
-  def hello do
-    :world
+  def to_ast(text, _opts) do
+    []
+  end
+
+  @doc """
+  Convert Markdown text directly to HTML
+  """
+  def render(text, _opts) do
+    text
   end
 end
